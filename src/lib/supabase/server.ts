6ed2419e29670c0710ca02937e5f35185c2ccd4f
MicheMK-TEC <migaelschneider@gmail.com>
2026-08-28 11:16:10 -0500

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -12,14 +12,13 @@ export function createClient() {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet: Array<{ name: string; value: string; options: any }>) {
+        setAll(cookiesToSet: any[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
             )
           } catch {
-            // El método setAll se llamó desde un Server Component.
-            // Esto se puede ignorar si tienes middleware refrescando las sesiones.
+            // Ignorado en componentes de servidor
           }
         },
       },
