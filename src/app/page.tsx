e4755c27063be5878c3b860e9d4e6c2cacb95f29
MicheMK-TEC <migaelschneider@gmail.com>
2026-08-29 19:54:14 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,2 @@
-import { redirect } from 'next/navigation' export default function Home() { redirect('/login') }
+import { redirect } from 'next/navigation';
+export default function Home() { redirect('/login'); }
